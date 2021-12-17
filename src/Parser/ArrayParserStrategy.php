4c07f96583550ee5a1d5debd83a705d0dfbb6b9d
moelkomy <mo.ah.elkomy@gmail.com>
2021-12-17 17:44:40 +0200

    Fix showing last record

--- src/Parser/ArrayParserStrategy.php
+++ src/Parser/ArrayParserStrategy.php
@@ -14,6 +14,7 @@ class ArrayParserStrategy implements ParserStrategyInterface
     public function parse(string $path): array
     {
         $lines   = file($path);
+        $lines[] = "\n"; // add empty line in the end to show last record
 
         $subs    = [];
 
